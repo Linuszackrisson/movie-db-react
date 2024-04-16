40f3a84e2dec58d469d68dbba2b1deda8a712d76
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2024-04-16 14:26:18 +0200

    Created functions for fetching random movies, and for searching manually

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx'
 import './index.css'
+import TestComponent from './test.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
