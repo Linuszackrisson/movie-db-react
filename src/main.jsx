ca53ffe96e68826ee15a4efffbdbf55f20408b09
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2024-04-18 19:59:32 +0200

    Added back the css import to main.jsx I removed by accident

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';; // Import createRoot from react-
 import { Provider } from 'react-redux';
 import store from '../redux/store'; // Import your Redux store
 import App from './App';
+import './index.css'
 
 // Use createRoot instead of ReactDOM.render
 createRoot(document.getElementById('root')).render(
